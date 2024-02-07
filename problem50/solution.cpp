d6815dd75c924d766d9431f2b41710b8e558a6a7
Davit Petrosyan <90375941+dpetrosy@users.noreply.github.com>
2024-02-07 17:32:44 +0400

    Create solution.cpp for problem 50

---
+++ problem50/solution.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    double myPow(double x, int n)
+    {
+        if (n < 0)
+            x = 1 / x;
+
+        double pow = 1;
+        long num = std::abs(n);
+        while (num)
+        {
+            if (num & 1)
+                pow *= x;
+            x *= x;
+            num = num >> 1;
+        }
+        return pow;
+    }
+};
