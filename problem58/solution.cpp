187b119e2920abeb2c8e141cdddda07d55ba6d7d
Davit Petrosyan <90375941+dpetrosy@users.noreply.github.com>
2024-02-09 19:39:22 +0400

    Create solution.cpp for problem 58

---
+++ problem58/solution.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    int lengthOfLastWord(string s)
+    {
+        int len = s.length() - 1;
+        int count = 0;
+
+        while (s[len] == ' ')
+            --len;
+
+        while (len >= 0 && s[len--] != ' ')
+            ++count;
+        return count;
+    }
+};
