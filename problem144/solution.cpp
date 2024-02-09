3c4263aef12aad26782ad7a2e5c72a391863dad2
Davit Petrosyan <90375941+dpetrosy@users.noreply.github.com>
2024-02-09 17:05:28 +0400

    Create solution.cpp for problem 144

---
+++ problem144/solution.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution
+{
+public:
+    void preorder(TreeNode* root, std::vector<int>& vec)
+    {
+        if (!root)
+            return;
+
+        vec.push_back(root->val);
+        preorder(root->left, vec);
+        preorder(root->right, vec);
+    }
+
+    vector<int> preorderTraversal(TreeNode* root)
+    {
+        std::vector<int> vec;
+        preorder(root, vec);
+        return vec;
+    }
+};
