9a55b24c3a0f37719622723c1f7fb1bc304ac0e8
Davit Petrosyan <90375941+dpetrosy@users.noreply.github.com>
2024-06-20 01:00:50 +0400

    Create solution.cpp for problem 27

---
+++ problem27/solution.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int removeElement(vector<int>& nums, int val)
+    {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right)
+        {
+            if (nums[left] != val)
+                ++left;
+            else if (nums[right] == val)
+                --right;
+            else
+                swap(nums[left++], nums[right--]);
+        }
+        return left;
+    }
+};
